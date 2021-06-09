7e24813c5ec4364c39b40e98b400bc26759ca36c
Igor Ponchielli <42591808+IgorFPonchielli@users.noreply.github.com>
2021-06-09 20:36:35 -0300

    aula web - 09062021

--- src/route.js
+++ src/route.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
 import HelloMessage from './HelloMessage';
 import Teste from './paginas/Teste';
+import Livro from './paginas/Livro';
+import ListarLivro from './paginas/ListarLivros';
+import CadLivro from './paginas/CadLivros';
+import Usuario from './paginas/Usuario'
 
 function Routes() {
     return (
@@ -10,6 +14,10 @@ function Routes() {
             <Switch>
                 <Route path="/" exact={true} component={HelloMessage}/>
                 <Route path="/teste" component={Teste}/>
+                <Route path="/listarlivros" component={Livro}/>
+                <Route path="/listarlivros2" component={ListarLivro}/>
+                <Route path="/cadlivros" component={CadLivro}/>
+                <Route path="/usuario" component={Usuario}/>
             </Switch>
         </BrowserRouter>
     )
